2bd763357bc4f640f7cc9e31c232e3a27cd7ec51
BartiWhite <102719705+BartiWhite@users.noreply.github.com>
2022-06-09 10:44:37 +0200

    Update Q_teleportationHost.py

--- Q_teleportationHost.py
+++ Q_teleportationHost.py
@@ -1,7 +1,7 @@
 import qsharp
-from Q_Teleportation import Teleportation
+from Q_Teleportation import TeleportationResult
 from Q_Teleportation import Main
 # here we type the message we wish to send (1/true or 0/false)
-print(Teleportation.simulate(sentMessage = 1))
-# here we can set the number of attempts that program will simulate with random messeges
+print(TeleportationResult.simulate(sentMessage = 1))
+# he we can set the number of attempts that program will simulate with random messeges
 print(Main.simulate(numberOfAttempts = 10000))
