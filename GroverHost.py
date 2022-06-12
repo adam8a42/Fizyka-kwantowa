624d3b123e90650e506da5e20cc36f14af68ff57
BartiWhite <102719705+BartiWhite@users.noreply.github.com>
2022-06-12 10:47:54 +0200

    Update GroverHost.py

--- GroverHost.py
+++ GroverHost.py
@@ -1,7 +1,10 @@
 import qsharp
+
 from Grover import runSudokuSearch
 print(runSudokuSearch.simulate(n = 2))
-from Grover import TestSudokuOracle
-print(TestSudokuOracle.simulate())
-from Grover import runTestSearch
-print(runTestSearch.simulate(n = 1))
+
+#from Grover import TestSudokuOracle
+#print(TestSudokuOracle.simulate())
+
+#from Grover import runTestSearch
+#print(runTestSearch.simulate(n = 1))
