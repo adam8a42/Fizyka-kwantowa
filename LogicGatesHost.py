e8b05938a1526c60deb255053a1642729bfccc70
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2022-06-12 11:19:07 +0200

    Revert "Update LogicGatesHost.py"
    This reverts commit 73208f5c717af9de31cb2c75904d0f699dbf9b6a.

--- LogicGatesHost.py
+++ LogicGatesHost.py
@@ -1,19 +1,14 @@
 import qsharp
-
 from LogicGates import TestEntanglement
-print(TestEntanglement.simulate(n = 10000))
-
+print(TestEntanglement.simulate())
 from LogicGates import Deutsch_Jozsa
-print(Deutsch_Jozsa.simulate(n = 2, type = 2))
-print(Deutsch_Jozsa.simulate(n = 3, type = 2))
-print(Deutsch_Jozsa.simulate(n = 4, type = 2))
-print(Deutsch_Jozsa.simulate(n = 5, type = 2))
-print(Deutsch_Jozsa.simulate(n = 2, type = 1))
-print(Deutsch_Jozsa.simulate(n = 3, type = 0))
-print(Deutsch_Jozsa.simulate(n = 4, type = 1))
-print(Deutsch_Jozsa.simulate(n = 5, type = 0))
-
+for j in range(3):
+    for i in range(1,6):
+        print(f"{j=}, result = {Deutsch_Jozsa.simulate(n = i, type = j)}")
 from LogicGates import Deutsch
-print(Deutsch.simulate(type = 0))
-print(Deutsch.simulate(type = 1))
-print(Deutsch.simulate(type = 2))
+print(f"type = 0, {Deutsch.simulate(type = 0)}")
+print(f"type = 1, {Deutsch.simulate(type = 1)}")
+print(f"type = 2, {Deutsch.simulate(type = 2)}")
+from LogicGates import funcTest
+
+
